64684782bdfd52156229d6d74ce59f162bbd4015
Lucas Crespo <ln.crespo@protonmail.com>
2021-05-13 13:50:35 +0200

    Change polybar config
    Theme skeleton by adi1090x:
    https://github.com/adi1090x/polybar-themes

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -1,6 +1,21 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c ~/.config/polybar/polybar --reload example &
+    MONITOR=$m polybar -c "$DIR"/config.ini --reload main &
   done
 else
   polybar -c ~/.config/polybar/polybar --reload example &
